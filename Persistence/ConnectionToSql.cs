b8d39a2a3889e09b09d23a3b1952e54b14409579
JavierGonzalez045 <javiergcasoperez@gmail.com>
2022-07-01 12:37:22 -0400

    Conexion a base de datos
    Agregue la clase de conexion a la base de datos Sql

---
+++ Persistence/ConnectionToSql.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Persistence
+{
+    public abstract class ConnectionToSql
+    {
+        private readonly string connectionString;
+
+        public ConnectionToSql()
+        {
+            connectionString = "Server=DESKTOP-GV2110Q;Database= MyCompany; integrated security= true";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
