92a2884afbe30ff2545f13d0d4033ce6df2543ff
JavierGonzalez045 <javiergcasoperez@gmail.com>
2022-07-06 18:01:40 -0400

    Termine el primer diseño del CustomTextBox

--- Presentation/Forms/Assets/CustomTextBox Design/JMCustomTextBox.Designer.cs
+++ Presentation/Forms/Assets/CustomTextBox Design/JMCustomTextBox.Designer.cs
@@ -29,21 +29,36 @@ namespace Presentation.Forms.Assets
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(7, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 17);
+            this.textBox1.TabIndex = 0;
             // 
             // JMCustomTextBox
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.DimGray;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "JMCustomTextBox";
-            this.Size = new System.Drawing.Size(193, 210);
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Size = new System.Drawing.Size(250, 30);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Presentation/Forms/Assets/CustomTextBox Design/JMCustomTextBox.cs
+++ Presentation/Forms/Assets/CustomTextBox Design/JMCustomTextBox.cs
@@ -12,9 +12,103 @@ namespace Presentation.Forms.Assets
 {
     public partial class JMCustomTextBox : UserControl
     {
+
+        // Fields
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlinedStyle = false;
+
+        // Constructor
         public JMCustomTextBox()
         {
             InitializeComponent();
+        }
+
+        // Properties
+        public Color BorderColor
+        {
+            get
+            {
+                return borderColor;
+            }
+
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+        public int BorderSize
+        {
+            get
+            {
+                return borderSize;
+            }
+
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        public bool UnderlinedStyle
+        {
+            get
+            {
+                return underlinedStyle;
+            }
+
+            set
+            {
+                underlinedStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        //Overridden methods
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+             
+            //Draw border
+            using (Pen penBorder = new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+                if (underlinedStyle) //Line Style
+                    graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                else //Normal Style
+                    graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (this.DesignMode)
+                UpdateControlHeight();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        // Private methods
+        private void UpdateControlHeight()
+        {
+            if (textBox1.Multiline == false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize = new Size(0, txtHeight);
+                textBox1.Multiline = false;
+                this.Height = textBox1.Height + this.Padding.Top + this.Padding.Bottom;
+            }
         }
     }
 }
